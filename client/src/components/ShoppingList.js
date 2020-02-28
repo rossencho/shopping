863f01cc294230723f0323ecfc005af557afab7f
Rosen Blagoev <rossencho@mail.ru>
2020-02-28 17:22:20 +0200

    getItems reducer

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { v4 as uuidv4 } from 'uuid';
+import {connect} from 'react-redux';
+import {getItems} from '../actions/itemActions';
+import PropTypes from 'prop-types';
+
 
 class ShoppingList extends Component {
-    state = {
-        items: [
-            {id: uuidv4(), name: 'Eggs'},
-            {id: uuidv4(), name: 'Bread'},
-            {id: uuidv4(), name: 'Walnuts'},
-            {id: uuidv4(), name: 'Tofu'}
-        ]
-    };
+
+    componentDidMount() {
+        this.props.getItems();
+    }
+  
     render() {
-        const {items} = this.state;
+        const {items} = this.props.item;
         return (
             <Container>
                 <Button 
@@ -61,4 +62,12 @@ class ShoppingList extends Component {
     }
 }
 
-export default ShoppingList;
+ShoppingList.propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired
+};
+
+const mapStateToPorps = (state) => ({
+    item: state.item
+});
+export default connect (mapStateToPorps,{getItems}) (ShoppingList);
